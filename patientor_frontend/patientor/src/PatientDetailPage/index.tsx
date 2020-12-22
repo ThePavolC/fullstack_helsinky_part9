c38c8618bd746aabd61f8847c7c9bc89d38f1afe
ThePavolC <celuch.pavol@gmail.com>
2020-12-22 22:31:24 +0100

    Exercise 9.22

--- patientor_frontend/patientor/src/PatientDetailPage/index.tsx
+++ patientor_frontend/patientor/src/PatientDetailPage/index.tsx
@@ -1,30 +1,132 @@
 import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Container, Icon, List } from "semantic-ui-react";
+import { Container, Icon, IconProps, List, Segment } from "semantic-ui-react";
 import { apiBaseUrl } from "../constants";
 import { addPatientDetail, useStateValue } from "../state";
-import { Entry, Gender, Patient } from "../types";
+import {
+  Entry,
+  Gender,
+  Patient,
+  HospitalEntry,
+  HealthCheckEntry,
+  OccupationalHealthcareEntry,
+  HealthCheckRating,
+} from "../types";
+import { assertNever } from "../utils";
 
-const EntryItem: React.FC<{ entry: Entry }> = ({ entry }) => {
+interface EntryDetailProps {
+  date: string;
+  description: string;
+  diagnosisCodes: Array<string> | undefined;
+  icon: IconProps["name"];
+  healthCheckRating?: HealthCheckRating;
+}
+
+const EntryDetail: React.FC<EntryDetailProps> = ({
+  date,
+  description,
+  diagnosisCodes,
+  icon,
+  healthCheckRating,
+}) => {
   const [{ diagnoses }] = useStateValue();
 
+  let heartColor: IconProps["color"];
+  switch (healthCheckRating) {
+    case HealthCheckRating.Healthy:
+      heartColor = "green";
+      break;
+    case HealthCheckRating.LowRisk:
+      heartColor = "yellow";
+      break;
+    case HealthCheckRating.HighRisk:
+      heartColor = "red";
+      break;
+    case HealthCheckRating.CriticalRisk:
+      heartColor = "black";
+      break;
+  }
+
   return (
-    <>
-      <p>
-        {entry.date} {entry.description}
-      </p>
-      {entry.diagnosisCodes && (
-        <List.List>
-          {entry.diagnosisCodes.map((code) => (
-            <List.Item key={code}>
-              {code} - {diagnoses[code]?.name}
-            </List.Item>
-          ))}
-        </List.List>
+    <Segment>
+      <h4>
+        {date} <Icon name={icon} />
+      </h4>
+
+      <div>
+        <i style={{ color: "dimgrey" }}>{description}</i>
+      </div>
+      {diagnosisCodes && (
+        <div>
+          <List.List>
+            {diagnosisCodes.map((code) => (
+              <List.Item key={code}>
+                {code} - {diagnoses[code]?.name}
+              </List.Item>
+            ))}
+          </List.List>
+        </div>
       )}
-    </>
+
+      {healthCheckRating !== undefined && (
+        <div>
+          <Icon name="heart" color={heartColor} />
+        </div>
+      )}
+    </Segment>
   );
+};
+
+const HospitalEntryDetail: React.FC<{ entry: HospitalEntry }> = ({ entry }) => {
+  return (
+    <EntryDetail
+      date={entry.date}
+      description={entry.description}
+      diagnosisCodes={entry?.diagnosisCodes}
+      icon="hospital"
+    />
+  );
+};
+
+const HealthCheckEntryDetail: React.FC<{ entry: HealthCheckEntry }> = ({
+  entry,
+}) => {
+  return (
+    <EntryDetail
+      date={entry.date}
+      description={entry.description}
+      diagnosisCodes={entry.diagnosisCodes}
+      icon="stethoscope"
+      healthCheckRating={entry.healthCheckRating}
+    />
+  );
+};
+
+const OccupationalHealthcareEntryDetail: React.FC<{
+  entry: OccupationalHealthcareEntry;
+}> = ({ entry }) => {
+  return (
+    <EntryDetail
+      date={entry.date}
+      description={entry.description}
+      diagnosisCodes={entry.diagnosisCodes}
+      icon="user md"
+    />
+  );
+};
+
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+  switch (entry.type) {
+    case "Hospital":
+      return <HospitalEntryDetail entry={entry} />;
+    case "HealthCheck":
+      return <HealthCheckEntryDetail entry={entry} />;
+    case "OccupationalHealthcare":
+      return <OccupationalHealthcareEntryDetail entry={entry} />;
+    default:
+      return assertNever(entry);
+  }
 };
 
 const PatientDetailPage: React.FC = () => {
@@ -72,7 +174,7 @@ const PatientDetailPage: React.FC = () => {
         <List>
           {patient?.entries.map((entry) => (
             <List.Item key={entry.id}>
-              <EntryItem entry={entry} />
+              <EntryDetails entry={entry} />
             </List.Item>
           ))}
         </List>
